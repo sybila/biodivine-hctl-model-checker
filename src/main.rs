bddf66b67f4fbdcd7d9076088754bff1adb7822d
ondrej33 <75424794+ondrej33@users.noreply.github.com>
2021-11-12 23:36:19 +0100

    started to employ evaluation in main

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@ mod operation_enums;
 mod implementation;
 mod evaluator;
 
-use tokenizer::{tokenize_recursive,print_tokens};
-use parser::parse_update_function;
+use tokenizer::{tokenize_recursive, print_tokens};
+use parser::parse_hctl_formula;
+use evaluator::{mark_duplicates, eval_node};
 
+use std::env;
+use std::fs::read_to_string;
+use std::convert::TryFrom;
+
+use biodivine_lib_param_bn::symbolic_async_graph::SymbolicAsyncGraph;
+use biodivine_lib_param_bn::BooleanNetwork;
 
 fn main() {
-    let formula : String = "!{x}: ~ EX {x}".to_string();
+    let formula : String = "~ EF coup_fti".to_string();
+    let filename : String = "models/[var5]__[id007]__[CORTICAL-AREA-DEVELOPMENT]/model.aeon".to_string();
     let tokens = match tokenize_recursive(&mut formula.chars().peekable(), true) {
         Ok(r) => r,
         Err(e) => {
@@ -17,13 +25,30 @@ fn main() {
             Vec::new()
         }
     };
-    print_tokens(&tokens);
+    // print_tokens(&tokens);
 
-    match parse_update_function(&tokens) {
+    match parse_hctl_formula(&tokens) {
         Ok(tree) => {
-            let t = *tree;
-            println!("{}", t.subform_str);
-            println!("{}", t.height)
+            println!("{}", tree.subform_str);
+            match read_to_string(filename) {
+                Ok(aeon_string) => {
+                    match BooleanNetwork::try_from(aeon_string.as_str()) {
+                        Ok(network) => {
+                            match SymbolicAsyncGraph::new(network) {
+                                Ok(graph) => {
+                                    let mut duplicates = mark_duplicates(&*tree);
+                                    eval_node(*tree, &graph, &mut duplicates);
+                                    // TODO - do something with it
+                                }
+                                Err(message) => println!("{}", message),
+                            }
+                        }
+                        Err(message) => println!("{}", message),
+                    }
+                }
+                Err(message) => println!("{}", message),
+            }
+
         },
         Err(message) => println!("{}", message),
     }
