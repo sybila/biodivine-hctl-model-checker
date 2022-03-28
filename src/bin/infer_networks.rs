dfcf74e05afe8663bbad2080592d546bca600a3d
ondrej33 <75424794+ondrej33@users.noreply.github.com>
2022-03-28 23:47:34 +0200

    Add option to work with steady states only

--- src/bin/infer_networks.rs
+++ src/bin/infer_networks.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 use std::path::Path;
 use hctl_model_checker::analysis::analyze_property;
 
+#[allow(dead_code)]
 fn create_attractor_formula(data_set: Vec<String>, forbid_extra_attr: bool) -> String {
     // basic version without forbidding additional attractors
     let mut formula = String::new();
@@ -11,9 +12,20 @@ fn create_attractor_formula(data_set: Vec<String>, forbid_extra_attr: bool) -> S
         if item.is_empty() {
             continue;
         }
-        formula.push_str(format!("(3{{x}}: (@{{x}}: {} & (!{{y}}: AG EF ({{y}} & {} )))) & ", item, item).as_str())
+
+        /*
+         We can create formula in two ways - components are either "!y: AG EF y" or "!y: AG EF (y and state)"
+         The latter is usually faster, but if we want to forbid other attractors, we must compute
+         the formula "!y: AG EF y" anyway and we can then just cache it
+         */
+        if forbid_extra_attr {
+            formula.push_str(format!("(3{{x}}: (@{{x}}: {} & (!{{y}}: AG EF {{y}} ))) & ", item).as_str())
+        }
+        else {
+            formula.push_str(format!("(3{{x}}: (@{{x}}: {} & (!{{y}}: AG EF ({{y}} & {} )))) & ", item, item).as_str())
+        }
     }
-    formula.push_str("true");
+    formula.push_str("true"); // just so we dont end with "&"
 
     // (optional) appendix for the formula which forbids additional attractors
     if forbid_extra_attr {
@@ -25,6 +37,33 @@ fn create_attractor_formula(data_set: Vec<String>, forbid_extra_attr: bool) -> S
             formula.push_str(format!("~(AG EF ( {} ))  & ", item).as_str())
         }
         formula.push_str("(!{y}: AG EF {y})))")
+    }
+
+    formula
+}
+
+#[allow(dead_code)]
+fn create_steady_state_formula(data_set: Vec<String>, forbid_extra_attr: bool) -> String {
+    // basic version without forbidding additional attractors
+    let mut formula = String::new();
+    for item in data_set.clone() {
+        if item.is_empty() {
+            continue;
+        }
+        formula.push_str(format!("(3{{x}}: (@{{x}}: {} & (!{{y}}: AX ({{y}})))) & ", item).as_str())
+    }
+    formula.push_str("true");
+
+    // (optional) appendix for the formula which forbids additional attractors
+    if forbid_extra_attr {
+        formula.push_str(" & ~(3{x}: (@{x}: ");
+        for item in data_set {
+            if item.is_empty() {
+                continue;
+            }
+            formula.push_str(format!("~( {} )  & ", item).as_str())
+        }
+        formula.push_str("(!{y}: AX {y})))")
     }
 
     formula
@@ -42,6 +81,9 @@ fn main() {
     let reader = BufReader::new(&data_file);
     let data: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
     let formula = create_attractor_formula(data, true);
+    //let formula = create_steady_state_formula(data, true);
+
+    println!("original formula: {}", formula.clone());
 
     let aeon_string = read_to_string(args[1].clone()).unwrap();
 
