5d498b95b86eac12b857b5bcc1896fb754be58c3
ondrej33 <75424794+ondrej33@users.noreply.github.com>
2022-04-01 20:04:32 +0200

    Add utility to check the inference result

--- src/bin/infer_networks.rs
+++ src/bin/infer_networks.rs
@@ -6,6 +6,7 @@ use std::fs::{read_to_string, File};
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::time::SystemTime;
+use biodivine_lib_param_bn::biodivine_std::traits::Set;
 
 use biodivine_lib_param_bn::symbolic_async_graph::SymbolicAsyncGraph;
 use biodivine_lib_param_bn::BooleanNetwork;
@@ -14,14 +15,16 @@ use biodivine_lib_param_bn::BooleanNetwork;
 /// Optimised version - first evaluates formula for specific attractor existence, then (if we want
 /// forbid all additional attractors) evaluates the formula for the attractor prohibition, this
 /// time only on graph with colors restricted to those from the first part
+/// If `goal_model` is not none, check whether its colors are included in the resulting set of colors
 fn perform_inference_with_attractors(
     data_set: Vec<String>,
     aeon_string: String,
     forbid_extra_attr: bool,
+    goal_aeon_string: Option<String>,
 ) {
     let start = SystemTime::now();
     let bn = BooleanNetwork::try_from(aeon_string.as_str()).unwrap();
-    let graph = SymbolicAsyncGraph::new(bn).unwrap();
+    let mut graph = SymbolicAsyncGraph::new(bn).unwrap();
 
     // first the part that ensures attractor existence
     let mut formula_part1 = String::new();
@@ -36,12 +39,13 @@ fn perform_inference_with_attractors(
     formula_part1.push_str("true"); // just so we dont end with "&"
     let results_formula1 = model_check_formula(formula_part1, &graph);
 
+    // take the colors as an (intermediate) result
+    let mut inferred_colors = results_formula1.colors();
+
     if forbid_extra_attr {
         // we will compute the part of formula which forbids additional attractors
         // we will use graph with colors RESTRICTED to those from formula 1
-        let colors_formula1 = results_formula1.colors();
-        let restricted_graph =
-            SymbolicAsyncGraph::new_restrict_colors_from_existing(graph, &colors_formula1);
+        graph = SymbolicAsyncGraph::new_restrict_colors_from_existing(graph, &inferred_colors);
 
         // appendix for the formula which forbids additional attractors
         let mut formula_part2 = String::new();
@@ -54,11 +58,31 @@ fn perform_inference_with_attractors(
         }
         formula_part2.push_str("false ))))");
 
-        let results_formula2 = model_check_formula(formula_part2, &restricted_graph);
+        let results_formula2 = model_check_formula(formula_part2, &graph);
+        inferred_colors = results_formula2.colors();
+    }
 
-        println!("{} colors (networks) found", results_formula2.colors().approx_cardinality())
+    println!("{} suitable networks found in total", inferred_colors.approx_cardinality());
+
+    // if the goal network was supplied, lets check whether it is part of the solution set
+    if let Some(goal_model) = goal_aeon_string {
+
+        let goal_bn = BooleanNetwork::try_from(goal_model.as_str()).unwrap();
+        match graph.mk_subnetwork_colors(&goal_bn) {
+            Ok(goal_colors) => {
+                // we will need intersection of goal colors with the ones from the result
+                let intersection_inferred_goal = goal_colors.intersect(&inferred_colors);
+                // if the goal is subset of result, it went well
+                if intersection_inferred_goal.approx_cardinality() == goal_colors.approx_cardinality() {
+                    println!("OK - color of goal network is included in resulting set.")
+                } else {
+                    println!("NOK - color of goal network is NOT included in resulting set.")
+                }
+            }
+            Err(e) => println!("{}", e),
+        }
     } else {
-        println!("{} colors (networks) found", results_formula1.colors().approx_cardinality())
+        println!("Goal network not provided.")
     }
     println!("Elapsed time: {}ms", start.elapsed().unwrap().as_millis());
 }
@@ -141,12 +165,16 @@ fn main() {
         _ => true  // we need match to be exhaustive
     };
 
+    // TODO: make this automatic from CLI
+    let goal_aeon_string = Some(read_to_string("inference_goal_model.aeon".to_string()).unwrap());
+    //let goal_model_aeon_string: Option<String> = None;
+
     let data_file = File::open(Path::new(args[2].as_str())).unwrap();
     let reader = BufReader::new(&data_file);
     let data: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
     let aeon_string = read_to_string(args[1].clone()).unwrap();
 
-    perform_inference_with_attractors(data, aeon_string, forbid_extra_attrs);
+    perform_inference_with_attractors(data, aeon_string, forbid_extra_attrs, goal_aeon_string);
 
     /*
     // older version:
