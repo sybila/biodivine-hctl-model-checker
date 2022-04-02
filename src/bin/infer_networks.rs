630e24a16a717008dcb8222c6ff6d1575ab0a35a
ondrej33 <75424794+ondrej33@users.noreply.github.com>
2022-04-02 15:31:11 +0200

    Add optimisation for inference computation

--- src/bin/infer_networks.rs
+++ src/bin/infer_networks.rs
@@ -25,48 +25,41 @@ fn perform_inference_with_attractors(
     let start = SystemTime::now();
     let bn = BooleanNetwork::try_from(aeon_string.as_str()).unwrap();
     let mut graph = SymbolicAsyncGraph::new(bn).unwrap();
+    let mut inferred_colors = graph.mk_unit_colors();
 
-    // first the part that ensures attractor existence
-    let mut formula_part1 = String::new();
+    // whole formula we want to eval is just a conjunction of smaller formulas
+    // "exists attractor_1" & ... & "exists attractor_n" & "NOT exists any other attractor"
+    // we will evaluate each conjunct only on colors where previous conjuncts hold
+
+    // first we evaluate the parts that ensure attractor(s) existence
     for attractor_state in data_set.clone() {
         if attractor_state.is_empty() {
             continue;
         }
-        formula_part1.push_str(
-            format!("(3{{x}}: (@{{x}}: {} & (AG EF ({})))) & ", attractor_state, attractor_state).as_str()
-        )
-    }
-    formula_part1.push_str("true"); // just so we dont end with "&"
-    let results_formula1 = model_check_formula(formula_part1, &graph);
-
-    // take the colors as an (intermediate) result
-    let mut inferred_colors = results_formula1.colors();
-
-    if forbid_extra_attr {
-        // we will compute the part of formula which forbids additional attractors
-        // we will use graph with colors RESTRICTED to those from formula 1
+        let formula = format!("(3{{x}}: (@{{x}}: {} & (AG EF ({}))))", attractor_state, attractor_state);
+        inferred_colors = model_check_formula(formula, &graph).colors();
+        // we now restrict the unit_colored_set in the graph object
         graph = SymbolicAsyncGraph::new_restrict_colors_from_existing(graph, &inferred_colors);
+    }
 
-        // appendix for the formula which forbids additional attractors
-        let mut formula_part2 = String::new();
-        formula_part2.push_str("~(3{x}: (@{x}: ~(AG EF (");
+    // if desired, we will add the formula which forbids additional attractors
+    if forbid_extra_attr {
+        let mut formula = String::new();
+        formula.push_str("~(3{x}: (@{x}: ~(AG EF (");
         for attractor_state in data_set {
             if attractor_state.is_empty() {
                 continue;
             }
-            formula_part2.push_str(format!("({}) | ", attractor_state).as_str());
+            formula.push_str(format!("({}) | ", attractor_state).as_str());
         }
-        formula_part2.push_str("false ))))");
-
-        let results_formula2 = model_check_formula(formula_part2, &graph);
-        inferred_colors = results_formula2.colors();
+        formula.push_str("false ))))"); // false is there just so it does not end with "|"
+        inferred_colors = model_check_formula(formula, &graph).colors();
     }
 
     println!("{} suitable networks found in total", inferred_colors.approx_cardinality());
 
     // if the goal network was supplied, lets check whether it is part of the solution set
     if let Some(goal_model) = goal_aeon_string {
-
         let goal_bn = BooleanNetwork::try_from(goal_model.as_str()).unwrap();
         match graph.mk_subnetwork_colors(&goal_bn) {
             Ok(goal_colors) => {
@@ -166,8 +159,8 @@ fn main() {
     };
 
     // TODO: make this automatic from CLI
-    let goal_aeon_string = Some(read_to_string("inference_goal_model.aeon".to_string()).unwrap());
-    //let goal_model_aeon_string: Option<String> = None;
+    //let goal_aeon_string = Some(read_to_string("inference_goal_model.aeon".to_string()).unwrap());
+    let goal_aeon_string: Option<String> = None;
 
     let data_file = File::open(Path::new(args[2].as_str())).unwrap();
     let reader = BufReader::new(&data_file);
@@ -176,12 +169,11 @@ fn main() {
 
     perform_inference_with_attractors(data, aeon_string, forbid_extra_attrs, goal_aeon_string);
 
+    // steady-state version:
     /*
-    // older version:
-    //let formula = create_attractor_formula(data.clone(), forbid_extra_attrs);
-    //let formula = create_steady_state_formula(data, forbid_extra_attrs);
-    //analyse_formula(aeon_string, formula, PrintOptions::ShortPrint);
-    //println!("original formula: {}", formula.clone());
+    let formula = create_steady_state_formula(data, forbid_extra_attrs);
+    analyse_formula(aeon_string, formula, PrintOptions::ShortPrint);
+    // println!("original formula: {}", formula.clone());
     // result should have 2^(number of vars) states - basically all states
      */
 }
