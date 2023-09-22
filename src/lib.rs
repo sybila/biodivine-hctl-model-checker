a9cb83d9a585650b264bcfdf663129b273f0dcf9
Ondřej Huvar <492849@mail.muni.cz>
2023-09-22 12:11:41 +0200

    Update instructions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //! A small library regarding analysis of dynamic properties of Boolean networks through HCTL model checking.
 //! As of now, the library supports:
 //!  - Model-checking analysis of HCTL properties on a (partially specified) BNs.
-//!  - Various formulae preprocessing utilities, such as tokenizing, parsing, or canonization.
+//!  - Various formulae preprocessing utilities, such as tokenizing, parsing, or some canonization.
 //!  - Manipulation with abstract syntactic trees for HCTL formulae.
-//!  - Searching for common sub-formulae across multiple formulae.
+//!  - Searching for common sub-formulae across multiple properties.
 //!  - Optimised evaluation for several patterns, such as various attractor types or reachability.
 //!  - Simultaneous evaluation of several formulae, sharing common computation via cache.
 //!
